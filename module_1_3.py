1b12902c1be8d70bc8365922c2bda25cd94a928a
Elena <strelen1983@gmail.com>
2024-12-17 18:03:51 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Elena'
+print (name, type (name))
+age = 41
+print (age, type (age))
+new_age = 1
+print (new_age, type (new_age))
+print (age + new_age)
+Is_student = 'True'
+print (Is_student, type (Is_student))
